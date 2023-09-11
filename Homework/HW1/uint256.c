3ec73572b7d83003bbeb846e3e95cc89bebe2188
GitRyers <rnguye14@jhu.edu>
2023-09-11 14:59:16 -0400

    implemented negation and subtraction

--- Homework/HW1/uint256.c
+++ Homework/HW1/uint256.c
@@ -36,20 +36,19 @@ UInt256 uint256_create(const uint32_t data[8]) {
 
 // Create a UInt256 value from a string of hexadecimal digits.
 UInt256 uint256_create_from_hex(const char *hex) {
-  UInt256 result = uint256_create_from_u32(0); 
+  UInt256 result = uint256_create_from_u32(0U); 
   int start; 
   if (strlen(hex) >= 8) {
     start = strlen(hex) - 8; 
   } else {
     start = 0;
   }
-  for (unsigned i = 0; start >= 0 && i < 8; ++i) {
+  for (unsigned i = 0; start >= 0 && i < 8; start -= 8, ++i) {
     char *str = (char *) hex;
     char buffer[9] = {'\0'};
     strncpy(buffer, str + start, 8);
     unsigned long val = strtoul(buffer, NULL, 16);
     result.data[i] = val;  
-    start -= 8; 
   }
   return result;
 }
@@ -80,7 +79,7 @@ uint32_t uint256_get_bits(UInt256 val, unsigned index) {
 
 // Compute the sum of two UInt256 values.
 UInt256 uint256_add(UInt256 left, UInt256 right) {
-  UInt256 sum = uint256_create_from_u32(0);
+  UInt256 sum = uint256_create_from_u32(0U);
   unsigned carry = 0;
   for (unsigned i = 0; i < 8; ++i) {
     uint32_t leftval = uint256_get_bits(left, i);
@@ -98,15 +97,18 @@ UInt256 uint256_add(UInt256 left, UInt256 right) {
 
 // Compute the difference of two UInt256 values.
 UInt256 uint256_sub(UInt256 left, UInt256 right) {
-  return uint256_add(left, uint256_negate(right)); 
+  UInt256 comp = uint256_negate(right); 
+  UInt256 diff = uint256_add(left, comp); 
+  return diff; 
 }
 
 // Return the two's-complement negation of the given UInt256 value.
 UInt256 uint256_negate(UInt256 val) {
-  UInt256 result;
-  //Figure out how you would invert the "bits" of a UInt256 value
-  //Does this mean inverting the bits of each uint32 value and adding 1? 
-  return result;
+  UInt256 result;  
+  for (unsigned i = 0; i < 8; ++i) {
+    result.data[i] = ~(uint256_get_bits(val, i)); 
+  }
+  return uint256_add(result, uint256_create_from_u32(1U)); 
 }
 
 // Return the result of rotating every bit in val nbits to
