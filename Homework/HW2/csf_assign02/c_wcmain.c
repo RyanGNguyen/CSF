18934aa9c4c1c377f0898e5c86d578e31ba7b842
John Liu <johnliu@johns-mbp.acasmart.jh.edu>
2023-09-23 18:44:04 -0400

    implemented void process_input_word in main

--- Homework/HW2/csf_assign02/c_wcmain.c
+++ Homework/HW2/csf_assign02/c_wcmain.c
@@ -5,7 +5,7 @@
 // Suggested number of buckets for the hash table
 #define HASHTABLE_SIZE 13249
 
-// TODO: prototypes for helper functions
+void process_input_word(struct WordEntry *buckets[], unsigned char *word, uint32_t *unique_words, const unsigned char **best_word, uint32_t *best_word_count);
 
 int main(int argc, char **argv) {
   // stats (to be printed at end)
@@ -14,7 +14,48 @@ int main(int argc, char **argv) {
   const unsigned char *best_word = (const unsigned char *) "";
   uint32_t best_word_count = 0;
 
-  // TODO: implement
+    FILE *file;
+    file = stdin;
+
+    // Initializing the hash table
+    // Will have arrays, each entry is a bucket
+    // Bucket points to list of words (hashed same value)
+    // All buckets will start empty
+
+    struct WordEntry *buckets[HASHTABLE_SIZE];
+    for (int i = 0; i < HASHTABLE_SIZE; i++) {
+        buckets[i] = NULL;
+    }
+
+    // Opens file for reading, can't use "read"
+
+    if (argc == 2) {
+        file = fopen(argv[1], "r");
+
+        if (file == NULL) {
+            perror("Error opening provided file");
+            return 1;
+        }
+    }
+
+    // Hold word that is being processed
+    unsigned char word[MAX_WORDLEN + 1];
+
+    // If word read from file, keep iterating
+    while (wc_readnext(file, word)) {
+        // Increase total word count
+        total_words++;
+
+        // Converts words to lower case and non alpha
+        // We want words to be case insensitive??
+        // non alpha means Cat & Cat! are same word
+        wc_tolower(word);
+        wc_trim_non_alpha(word);
+
+        // Adds the word to our hash table
+        // Updates unique word count & most frequent word stats
+        process_input_word(buckets, word, &unique_words, &best_word, &best_word_count);
+    }
 
   printf("Total words read: %u\n", (unsigned int) total_words);
   printf("Unique words read: %u\n", (unsigned int) unique_words);
