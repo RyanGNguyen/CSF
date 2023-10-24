3b226d3531bd2417e583c0d35d72560cb1dca3e1
Andy Tang <andytang0220@gmail.com>
2023-10-24 16:36:24 -0400

    csimfuncs.cpp

--- Homework/HW3/csimfuncs.cpp
+++ Homework/HW3/csimfuncs.cpp
@@ -132,11 +132,12 @@ void checkEviction(std::string arg2, std::string arg6) {
     }
 }
 
-std::uint32_t get_tag(std::uint32_t address, int setBits, int blockBits) {
+std::uint32_t get_tag(std::uint32_t address, int numSets, int blockBits) {
+    int setBits = log(numSets)/log(2);
     return address >> setBits + blockBits;
 }
 
 std::uint32_t get_set(std::uint32_t address,int numSets, int blockBits) {
-    int set_mask = numSets- 1;
+    int set_mask = numSets - 1;
     return (address >> blockBits) & set_mask;
 }
