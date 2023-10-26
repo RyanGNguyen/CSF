db0fc7ecf89a2fad55f770b9a631fee5be03e8a0
Andy Tang <andytang0220@gmail.com>
2023-10-25 21:41:44 -0400

    csimmain.cpp

--- Homework/HW3/csimmain.cpp
+++ Homework/HW3/csimmain.cpp
@@ -19,10 +19,10 @@ int main(int argc, char* argv[]) {
         std::uint32_t address = std::stoul(v[1], NULL, 16);  // Convert hex address to unsigned int
         unsigned byteBits = log2(cache.numBytes);                    // Calculate number of bits for byte offset
         unsigned setBits = log2(cache.numSets);                      // Calculate number of bits for set index
-        if (v[0].compare("l") == 0) { 
-            cache.load(address, byteBits, setBits);                          // Load address
+        if (v[0].compare("s") == 0) { 
+            cache.store(address, byteBits, setBits);                          // Store address
         } else {
-            cache.store(address, byteBits, setBits);                         // Store address
+            cache.load(address, byteBits, setBits);                         // Load address
         }
     }
     cache.print_statistics();                   // Print statistics
