5451051cc25500abea0cdd7a5c9adef4a6adb9b0
Andy Tang <andytang0220@gmail.com>
2023-11-09 18:40:27 -0500

    parsort.c

--- Homework/HW4/csf_assign04/parsort.c
+++ Homework/HW4/csf_assign04/parsort.c
@@ -72,26 +72,43 @@ void merge_sort(int64_t *arr, size_t begin, size_t end, size_t threshold) {
 
   // TODO: parallelize the recursive sorting
 
-  pid_t pid = fork();
-  if (pid == -1) { // fork failed to start a new process
+  pid_t pid1 = fork();
+  if (pid1 == -1) { // fork failed to start a new process
     fatal("fork failed to start a new process");
-  } else if (pid == 0) { // if pid is 0, we are in the child process
+  } else if (pid1 == 0) { // if pid is 0, we are in the child process
     merge_sort(arr, begin, mid, threshold); 
     exit(0);
-  }// parent process
-  int wstatus;
-  pid_t actual_pid = waitpid(pid, &wstatus, 0); // blocks until the child process completes
+  }
+
+  pid_t pid2 = fork();
+  if (pid2 == -1) {
+    fatal("fork failed to start a new process");
+  } else if (pid2 == 0) {
+    merge_sort(arr, mid, end, threshold);
+    exit(0);
+  }
+  int wstatus1;
+  pid_t actual_pid = waitpid(pid1, &wstatus1, 0); // blocks until the child process completes
   if (actual_pid == -1) {
     fatal("waitpid failure"); // handle waitpid failure
     }
-  if (!WIFEXITED(wstatus)) {
+  if (!WIFEXITED(wstatus1)) {
     fatal("subprocess crashed, was interrupted, or did not exit normally");
   }
-  if (WEXITSTATUS(wstatus) != 0) {
+  if (WEXITSTATUS(wstatus1) != 0) {
     fatal("subprocess returned a non-zero exit code");
   }
-  merge_sort(arr, mid, end, threshold);
-
+  int wstatus2;
+  pid_t actual_pid2 = waitpid(pid2, &wstatus2, 0);
+  if (actual_pid2 == -1) {
+    fatal("waitpid failure");
+  }
+  if (!WIFEXITED(wstatus2)) {
+    fatal("subprocess crashed, was interrupted, or did not exit normally");
+  }
+  if (WEXITSTATUS(wstatus2) != 0) {
+    fatal("subprocess returned a non-zero exit code");
+  }
   // allocate temp array now, so we can avoid unnecessary work
   // if the malloc fails
   int64_t *temp_arr = (int64_t *) malloc(size * sizeof(int64_t));
