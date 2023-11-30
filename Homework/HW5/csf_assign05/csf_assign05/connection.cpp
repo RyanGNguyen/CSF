1f48effd9533eef77de56c2f86d80d1c668103d8
GitRyers <rnguye14@jhu.edu>
2023-11-30 16:15:29 -0500

    fixed send

--- Homework/HW5/csf_assign05/csf_assign05/connection.cpp
+++ Homework/HW5/csf_assign05/csf_assign05/connection.cpp
@@ -16,7 +16,7 @@ Connection::Connection(int fd)
   : m_fd(fd)
   , m_last_result(SUCCESS) {
   // TODO: call rio_readinitb to initialize the rio_t object
-  rio_readinitb(&m_fdbuf, m_fd); 
+  rio_readinitb(&m_fdbuf, fd); 
 }
 
 void Connection::connect(const std::string &hostname, int port) {
@@ -35,14 +35,14 @@ void Connection::connect(const std::string &hostname, int port) {
 
 Connection::~Connection() {
   // TODO: close the socket if it is open
-  if (Connection::is_open()) {
-    Connection::close();
+  if (is_open()) {
+    close();
   }
 }
 
 bool Connection::is_open() const {
   // TODO: return true if the connection is open
-  return m_fd > 0;
+  return m_fd >= 0;
 }
 
 void Connection::close() {
@@ -54,25 +54,11 @@ bool Connection::send(const Message &msg) {
   // TODO: send a message
   // return true if successful, false if not
   // make sure that m_last_result is set appropriately
-  rio_writen(m_fd, msg.tag.c_str(), msg.tag.length()); 
-  rio_writen(m_fd, ":", 1);
-  rio_writen(m_fd, msg.data.c_str(), msg.data.length());
-  rio_writen(m_fd, "\n", 1);
-  
-  // Unsure
-  Message reply;
-  if (Connection::receive(reply)) { 
-    if (reply.tag == TAG_ERR) {
-      m_last_result = EOF_OR_ERROR;
-      std::cerr << reply.data << "\n";
-      if (msg.tag == TAG_SLOGIN or msg.tag == TAG_RLOGIN) {
-        exit(EOF_OR_ERROR);
-      }
-      return false;
-    } else {
-      m_last_result = SUCCESS;
-      return true;
-    }
+  std::string buffer = msg.tag + ":" + msg.data + "\n";
+  ssize_t n = (m_fd, buffer.c_str(), buffer.length()); 
+  if (n == buffer.length()) {
+    m_last_result = SUCCESS;
+    return true;
   } else {
     m_last_result = EOF_OR_ERROR;
     return false;
@@ -86,7 +72,7 @@ bool Connection::receive(Message &msg) {
   char buf[Message::MAX_LEN];
   ssize_t n = rio_readlineb(&m_fdbuf, buf, Message::MAX_LEN);
   
-  if (n > 0) {
+  if (n == Message::MAX_LEN) {
     char* tag;
     char* data;
     tag = strtok_r(buf, ":", &data);
